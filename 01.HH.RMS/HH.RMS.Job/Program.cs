f98223b679c7923ece7cee0e1fbff6d22c411fb1
henry <Henry.HUANG@edenred.com>
2016-10-25 13:30:02 +0800

    11111111

--- 01.HH.RMS/HH.RMS.Job/Program.cs
+++ 01.HH.RMS/HH.RMS.Job/Program.cs
@@ -1,6 +1,10 @@
-﻿using HH.RMS.Common.Unity;
+﻿using HH.RMS.Common.Constant;
+using HH.RMS.Common.Unity;
 using HH.RMS.Scheduler;
 using HH.RMS.Service.Web.Interface;
+using log4net.Appender;
+using log4net.Config;
+using log4net.Repository;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,14 +15,35 @@ namespace HH.RMS.Job
 {
     class Program
     {
+        public const string configName = "\\HH.RMS.Job.exe.config";
         static void Main(string[] args)
         {
-
+            LogRegister();
             new UnityScheduler(UnityManager.instance);
             SchedulerManager manager = new SchedulerManager();
             manager.Initialize();
             
             Console.Write("Job Excetue");
+        }
+        public static void LogRegister()
+        {
+            string configfile = AppDomain.CurrentDomain.BaseDirectory + configName;
+            System.IO.FileInfo fileInfo = new System.IO.FileInfo(configfile);
+            XmlConfigurator.ConfigureAndWatch(fileInfo);
+            ILoggerRepository hier = log4net.LogManager.GetRepository();
+            if (hier != null)
+            {
+                foreach (log4net.Appender.IAppender appender in hier.GetAppenders())
+                {
+                    if (appender.Name != "ADONetAppender")
+                    {
+                        continue;
+                    }
+                    AdoNetAppender adoAppender = (AdoNetAppender)appender;
+                    adoAppender.ConnectionString = Config.sqlConnStr;
+                    adoAppender.ActivateOptions();
+                }
+            }
         }
     }
 }
