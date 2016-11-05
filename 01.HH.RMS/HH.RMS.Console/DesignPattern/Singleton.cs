9d54e81638071015fb7eb4724ec703b9cf2dc579
henry0322 <h0322@163.com>
2016-11-05 18:56:21 +0800

    21

---
+++ 01.HH.RMS/HH.RMS.Console/DesignPattern/Singleton.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HH.RMS.Console.DesignPattern
+{
+    public sealed class Singleton
+    {
+        private static readonly Singleton _instance = new Singleton();
+        private Singleton()
+        { 
+        }
+        public Singleton instance
+        {
+            get { return _instance; }
+        }
+    }
+    public sealed class Singleton2
+    {
+        private static Singleton2 _instance = null;
+        static object obj = new object();
+        Singleton2()
+        { }
+        public static Singleton2 instance
+        {
+            get {
+                if(_instance==null)
+                { 
+                    lock (obj)
+                    {
+                        if (_instance == null)
+                        {
+                            _instance = new Singleton2();
+                        }
+                    }
+                }
+                return _instance;
+            }
+        }
+    }
+}
---
+++ 01.HH.RMS/HH.RMS.Console/Wechat/SendMessage.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HH.RMS.Wechat;
+using HH.RMS.Wechat.Message;
+using HH.RMS.Wechat.Model;
+
+namespace HH.RMS.Console.Wechat
+{
+    public class SendMessage
+    {
+        public SendMessage()
+        {
+            string postString = "<xml><ToUserName><![CDATA[gh_f9717ac9e758]]></ToUserName> <FromUserName><![CDATA[o2Xz5s7gsaUAVuk_lte_iOPdDPwk]]></FromUserName> <CreateTime>1467876889</CreateTime> <MsgType><![CDATA[text]]></MsgType> <Content><![CDATA[明星产品]]></Content> <MsgId>6304483233223097116</MsgId> </xml>";
+            MessageManager.instance.Manager(postString);
+
+        }
+
+        
+
+    }
+    public class SendMessageDefine : ProcessText
+    {
+        public SendMessageDefine()
+        {
+            string postString = "<xml><ToUserName><![CDATA[gh_f9717ac9e758]]></ToUserName> <FromUserName><![CDATA[o2Xz5s7gsaUAVuk_lte_iOPdDPwk]]></FromUserName> <CreateTime>1467876889</CreateTime> <MsgType><![CDATA[text]]></MsgType> <Content><![CDATA[明星产品]]></Content> <MsgId>6304483233223097116</MsgId> </xml>";
+            MessageManager.instance.Manager(postString);
+        }
+        public override void ProcessMsg(WechatPostModel model)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
